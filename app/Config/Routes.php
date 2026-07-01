5bb6f8f1c42bbf6ecb00d1f913d0d0e77a0a2e7f
AdiwenoRS <adiwenors@gmail.com>
2026-07-02 00:48:44 +0700

    Chore: Add auth routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,11 +3,19 @@
 use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
-$routes->get('/', 'Home::index');
-$routes->get('employees', 'EmployeeController::index');
-$routes->get('employees/create', 'EmployeeController::create');
-$routes->post('employees/store', 'EmployeeController::store');
-$routes->get('employees/edit/(:num)', 'EmployeeController::edit/$1');
-$routes->post('employees/update/(:num)', 'EmployeeController::update/$1');
-$routes->get('employees/delete/(:num)', 'EmployeeController::delete/$1');
-$routes->get('employees/export', 'EmployeeController::export');
+// Authentication Forms & Actions (Public access)
+$routes->get('auth', 'AuthController::index');
+$routes->post('auth/login', 'AuthController::login');
+$routes->get('auth/logout', 'AuthController::logout');
+
+// Protected Routes Group (Protected by 'auth' filter)
+$routes->group('', ['filter' => 'auth'], function($routes) {
+    $routes->get('/', 'EmployeeController::index');
+    $routes->get('employees', 'EmployeeController::index');
+    $routes->get('employees/create', 'EmployeeController::create');
+    $routes->post('employees/store', 'EmployeeController::store');
+    $routes->get('employees/edit/(:num)', 'EmployeeController::edit/$1');
+    $routes->post('employees/update/(:num)', 'EmployeeController::update/$1');
+    $routes->get('employees/delete/(:num)', 'EmployeeController::delete/$1');
+    $routes->get('employees/export', 'EmployeeController::export');
+});
